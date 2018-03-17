ddf7d4732a796ccc8f93c2da5394fadd17a116a6
FlorianVaissiere <florian.vaissiere@orange.fr>
2018-03-17 18:45:14 +0100

    Score fonctionnel

---
+++ src/model/ScoreFile.java
@@ -0,0 +1,101 @@
+package model;
+
+import jdk.jfr.StackTrace;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class ScoreFile {
+
+    final String chemin = "saveScore/score.txt";
+    final File fichier =new File(chemin);
+
+    public String scores[] = new String[65535];
+    public int taille;
+
+    public ScoreFile(){
+        try {
+            fichier.createNewFile();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void writeScore(String nom, int score){
+        try {
+            fichier .createNewFile();
+            FileWriter writer = new FileWriter(fichier);
+            try {
+                writer.write(nom + ":" + score + "\n");
+            } finally {
+                writer.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readScore(){
+        int i = 0;
+        try {
+            fichier.createNewFile();
+            FileReader score = new FileReader(fichier);
+            BufferedReader bfr = new BufferedReader(score);
+            try{
+                String line = bfr.readLine();
+                setScores(line, 0);
+
+                while(line != null){
+                    line = bfr.readLine();
+                    setScores(line, i);
+                    i++;
+                }
+                bfr.close();
+                score.close();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void orderScore(){
+        String parti[];
+        String parti2[];
+        int partsi  = 0;
+        int partsis = 0 ;
+        readScore();
+        for (int i = 0 ; i < 65535 ; i++){
+            if(i < 65535 && scores[i] != null){
+                parti        = scores[i].split(":");
+                partsi      = Integer.parseInt(parti[1]);
+
+                if(scores[i+1] != null){
+                    parti2        = scores[i+1].split(":");
+                    partsis     = Integer.parseInt(parti2[1]);
+                }
+
+                if (partsi < partsis){
+                    String tampon;
+                    tampon = scores[i];
+                    scores[i] = scores[i+1];
+                    setScores(scores[i], i);
+                    scores[i+1] = tampon;
+                    setScores(scores[i+1], i+1);
+                    System.out.println(scores[i]);
+                }
+            }
+        }
+    }
+
+    public void setScores(String line, int i){
+        this.scores[i] = line;
+       // System.out.println(scores[i]);
+    }
+
+}
--- src/vue/FenetreScore.java
+++ src/vue/FenetreScore.java
@@ -3,6 +3,7 @@ package vue;
 import controleur.ControlFenetreScores;
 import model.BDD;
 import model.Jeu;
+import model.ScoreFile;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,6 +17,11 @@ public class FenetreScore extends JPanel {
     private Image imageFenetreScore;
 
     public JButton retour;
+
+    public int x1 = 250;
+    public int y1 = 250;
+
+    ScoreFile file = new ScoreFile();
 
 
     public FenetreScore() {
@@ -40,11 +46,37 @@ public class FenetreScore extends JPanel {
         super.paintComponent(g);
         g.drawImage(imageFenetreScore, 0, 0, getWidth(), getHeight(), this);
 
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setFont(new Font("TimesRoman",Font.PLAIN, 30));
-        g2.setColor(Color.white);
-        g2.drawString(jeu.score[0],150,100);
+        file.orderScore();
+
+        Graphics2D g1 = (Graphics2D) g;
+        g1.setFont(new Font("TimesRoman",Font.PLAIN, 20));
+        g1.setColor(Color.white);
+        g1.drawString("Scores",300, 125);
+
+
+        for (int i = 0; i < 65535; i++){
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setFont(new Font("TimesRoman",Font.PLAIN, 20));
+            g2.setColor(Color.white);
+            if (file.scores[i]!=null){
+                if(i == 0){
+                    g2.drawString(file.scores[i], x1, y1);
+                } else {
+                    setCoordY(getCoordY());
+                    g2.drawString(file.scores[i], x1, getCoordY());
+                }
+            }
+        }
 
         retour.setBounds(Fenetre.adapterResolutionEnX(64), Fenetre.adapterResolutionEnY(985), Fenetre.adapterResolutionEnX(256), Fenetre.adapterResolutionEnY(41));
     }
+
+    public void setCoordY(int i){
+        this.y1 = y1+30;
+    }
+
+    public int getCoordY(){
+        return y1;
+    }
+
 }
